c18b686e1a36dfd38ea4d4a0e6f1818086b8cd91
OnlineAlex <becasualua@gmail.com>
2018-03-15 22:05:10 +0200

    удалил get_dict_bars(), вынес код в ifmain все кавычки теперь '' заменил цикл в get_closest_bar() на min() добавил ссылку на формулу расчета дистанции

--- bars.py
+++ bars.py
@@ -1,48 +1,39 @@
+import sys
 import json
 from math import radians, cos, sin, asin, sqrt
 
 
-def get_dict_bars(bars_json):
-    try:
-        bars_dict = load_data(bars_json)
-    except FileNotFoundError:
-        print('Ошибка! Система не нашла такой файл.')
-        print('Пробуйте указать полный путь к файлу.')
-    except ValueError:
-        print('Ошибка. Файл должен быть в формате JSON.')
-    else:
-        return bars_dict['features']
-
-
 def load_data(filepath):
-    with open(filepath, 'r', encoding="utf8") as bars_file:
-        return json.load(bars_file)
+    with open(filepath, 'r', encoding='utf8') as bars_file:
+        return json.load(bars_file)['features']
 
 
 def get_biggest_bar(bars_info):
-    biggest_bar = max(bars_info, key=lambda bar: bar['properties']['Attributes']['SeatsCount'])
-    return biggest_bar
+    biggest_bar = max(bars_info,
+                      key=lambda bar: bar['properties']
+                                         ['Attributes']
+                                         ['SeatsCount'])
+
+    return biggest_bar['properties']['Attributes']['Name']
 
 
 def get_smallest_bar(bars_info):
-    smallest_bar = min(bars_info, key=lambda bar: bar['properties']['Attributes']['SeatsCount'])
-    return smallest_bar
+    smallest_bar = min(bars_info,
+                       key=lambda bar: bar['properties']
+                                          ['Attributes']
+                                          ['SeatsCount'])
+
+    return smallest_bar['properties']['Attributes']['Name']
 
 
 def get_closest_bar(bars_info, user_lon, user_lat):
-    bars_distance = {}
-    for bar in bars_info:
-        bar_lon = bar['geometry']['coordinates'][1]
-        bar_lat = bar['geometry']['coordinates'][0]
-        bar_distance = calculates_distance(user_lon,
-                                           user_lat,
-                                           bar_lon,
-                                           bar_lat)
-        bars_distance[bar['properties']['Attributes']['Name']] = bar_distance
+    closest_bar = min(bars_info,
+                      key=lambda bar: calculates_distance(user_lon,
+                                                          user_lat,
+                                                          bar['geometry']['coordinates'][1],
+                                                          bar['geometry']['coordinates'][0]))
 
-    closest_bar = min(bars_distance.items(), key=lambda bar: bar[1])
-
-    return closest_bar[0]
+    return closest_bar['properties']['Attributes']['Name']
 
 
 def calculates_distance(lon1, lat1, lon2, lat2):
@@ -50,35 +41,47 @@ def calculates_distance(lon1, lat1, lon2, lat2):
     """
     Функция считает растояние в КМ между двумя GPS точками.
     Координаты должны быть во float.
+    Формула https://en.wikipedia.org/wiki/Haversine_formula
     """
 
-    lon1, lat1, lon2, lat2 = map(radians, (lon1, lat1, lon2, lat2))
+    earth_radius = 6371
+    lon1, lat1, lon2, lat2 = map(radians, (lon1,
+                                           lat1,
+                                           lon2,
+                                           lat2))
     d_lon = lon2 - lon1
     d_lat = lat2 - lat1
-    a = sin(d_lat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(d_lon / 2) ** 2
+    a = sin(d_lat / 2) ** 2 + \
+        cos(lat1) * cos(lat2) * sin(d_lon / 2) ** 2
     c = 2 * asin(sqrt(a))
-    km = 6367 * c
+    km = earth_radius * c
     return km
 
 
 if __name__ == '__main__':
-    bars_data_file = input('Напишите путь к файлу c данными о барах: \n'
-                           'Скачать его можно по ссылке data.mos.ru/opendata/7710881420-bary \n')
-    bars_data = get_dict_bars(bars_data_file)
-
-    name_biggest_bar = get_biggest_bar(bars_data)['properties']['Attributes']['Name']
-    print('Самый большой бар — {}'.format(name_biggest_bar))
-
-    name_smallest_bar = get_smallest_bar(bars_data)['properties']['Attributes']['Name']
-    print('Самый маленький бар — {}'.format(name_smallest_bar))
-
-    print('\nСейчас я найду ближайший к вам бар')
-
     try:
-        longitude = float(input('Широта на которой вы находитесь:'))
-        latitude = float(input('Широта на которой вы находитесь:'))
+        bars_data_file = sys.argv[1]
+        bars_data = load_data(bars_data_file)
+    except IndexError:
+        print('Ошибка! Вы не указали путь к файлу JSON.')
+        print('Сработает, если написать "python bars.py <путь к файлу>"')
+    except FileNotFoundError:
+        print('Ошибка! Система не нашла такой файл.')
+        print('Пробуйте указать полный путь к файлу.')
     except ValueError:
-        print('Координты введены не верно. Пишите только цифры. '
-              'Напр: "55.9862994"')
+        print('Ошибка. Файл должен быть в формате JSON.')
     else:
-        print('Ближайший бар — {}'.format(get_closest_bar(bars_data, longitude, latitude)))
+        print('Самый большой бар — {}'.format(get_biggest_bar(bars_data)))
+        print('Самый маленький бар — {}'.format(get_smallest_bar(bars_data)))
+
+        print('\nСейчас я найду ближайший к вам бар')
+        try:
+            longitude = float(input('Широта на которой вы находитесь:'))
+            latitude = float(input('Долгота на которой вы находитесь:'))
+        except ValueError:
+            print('Координты введены не верно. Пишите только цифры.'
+                  'Напр: "55.9862994"')
+        else:
+            print('Ближайший бар — {}'.format(get_closest_bar(bars_data,
+                                                              longitude,
+                                                              latitude)))
