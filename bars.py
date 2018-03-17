70da75290c6c3b9dd02c5a6cdb99f4cd4268cde2
OnlineAlex <becasualua@gmail.com>
2018-03-17 14:00:08 +0200

    - переписал функции. Теперь возвращают всю информацию о барах - отформатировал длинные строки.

--- bars.py
+++ bars.py
@@ -8,38 +8,59 @@ def load_data(filepath):
         return json.load(bars_file)['features']
 
 
-def main(bars_info):
-    print('Самый большой бар — {}'.format(get_biggest_bar(bars_info)))
-    print('Самый маленький бар — {}'.format(get_smallest_bar(bars_info)))
-
-    print('Сейчас я найду ближайший к вам бар')
+def main():
     try:
-        user_longitude, user_latitude = get_user_location()
+        bars_data_file = sys.argv[1]
+        bars_info = load_data(bars_data_file)
+    except IndexError:
+        print('Укажите путь к файлу JSON.')
+    except FileNotFoundError:
+        print('Файл не найден')
     except ValueError:
-        print('Координты введены не верно. Пишите только цифры.'
-              'Напр: "55.9862994"')
+        print('Ошибка. Файл должен быть в формате JSON.')
     else:
-        print('Ближайший бар — {}'.format(get_closest_bar(bars_info,
-                                                          user_longitude,
-                                                          user_latitude)))
+        print('Самый большой бар — {}'.format(
+            get_biggest_bar(bars_info)['properties']['Attributes']['Name']
+        ))
+
+        print('Самый маленький бар — {}'.format(
+            get_smallest_bar(bars_info)['properties']['Attributes']['Name']
+        ))
+
+        print('Сейчас я найду ближайший к вам бар')
+        try:
+            user_longitude, user_latitude = get_user_location()
+        except ValueError:
+            print('Координты введены не верно. Пишите только цифры.'
+                  'Напр: "55.9862994"')
+        else:
+            closest_bar = get_closest_bar(
+                bars_info,
+                user_longitude,
+                user_latitude
+            )
+
+            print('Ближайший бар — {}'.format(
+                closest_bar['properties']['Attributes']['Name']
+            ))
 
 
 def get_biggest_bar(bars_info):
-    biggest_bar = max(bars_info,
-                      key=lambda bar: bar['properties']
-                                         ['Attributes']
-                                         ['SeatsCount'])
+    biggest_bar = max(
+        bars_info,
+        key=lambda bar: bar['properties']['Attributes']['SeatsCount']
+    )
 
-    return biggest_bar['properties']['Attributes']['Name']
+    return biggest_bar
 
 
 def get_smallest_bar(bars_info):
-    smallest_bar = min(bars_info,
-                       key=lambda bar: bar['properties']
-                                          ['Attributes']
-                                          ['SeatsCount'])
+    smallest_bar = min(
+        bars_info,
+        key=lambda bar: bar['properties']['Attributes']['SeatsCount']
+    )
 
-    return smallest_bar['properties']['Attributes']['Name']
+    return smallest_bar
 
 
 def get_user_location():
@@ -49,13 +70,17 @@ def get_user_location():
 
 
 def get_closest_bar(bars_info, user_lon, user_lat):
-    closest_bar = min(bars_info,
-                      key=lambda bar: calculates_distance(user_lon,
-                                                          user_lat,
-                                                          bar['geometry']['coordinates'][1],
-                                                          bar['geometry']['coordinates'][0]))
+    closest_bar = min(
+        bars_info,
+        key=lambda bar: calculates_distance(
+            user_lon,
+            user_lat,
+            bar['geometry']['coordinates'][1],
+            bar['geometry']['coordinates'][0]
+        )
+    )
 
-    return closest_bar['properties']['Attributes']['Name']
+    return closest_bar
 
 
 def calculates_distance(lon1, lat1, lon2, lat2):
@@ -67,28 +92,18 @@ def calculates_distance(lon1, lat1, lon2, lat2):
     """
 
     earth_radius = 6371
-    lon1, lat1, lon2, lat2 = map(radians, (lon1,
-                                           lat1,
-                                           lon2,
-                                           lat2))
+    lon1, lat1, lon2, lat2 = map(
+        radians,
+        (lon1, lat1, lon2, lat2)
+    )
+
     d_lon = lon2 - lon1
     d_lat = lat2 - lat1
-    a = sin(d_lat / 2) ** 2 + \
-        cos(lat1) * cos(lat2) * sin(d_lon / 2) ** 2
+    a = sin(d_lat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(d_lon / 2) ** 2
     c = 2 * asin(sqrt(a))
     km = earth_radius * c
     return km
 
 
 if __name__ == '__main__':
-    try:
-        bars_data_file = sys.argv[1]
-        bars_data = load_data(bars_data_file)
-    except IndexError:
-        print('Укажите путь к файлу JSON.')
-    except FileNotFoundError:
-        print('Файл не найден')
-    except ValueError:
-        print('Ошибка. Файл должен быть в формате JSON.')
-    else:
-        main(bars_data)
+    main()
